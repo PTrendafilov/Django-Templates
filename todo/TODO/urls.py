253b323b565bd53d065720e509b05991e7321015
PTrendafilov <80783727+PTrendafilov@users.noreply.github.com>
2022-05-25 22:04:23 +0300

    Add files via upload
    Full CRUD functionality without CSS

--- todo/TODO/urls.py
+++ todo/TODO/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('create/', views.create, name='create'),
     path('create/task', views.create_task, name='create_task'),
+    path('complete/<int:id>/', views.complete_task, name='complete_task'),
+    path('delete/<int:id>/', views.delete, name='delete'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('edit/<int:id>/task', views.edit_task, name='edit_task'),
 ]
--- todo/TODO/views.py
+++ todo/TODO/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('id')
     return render(request, 'index.html', {'tasks':tasks})
 
 def create(request):
@@ -17,5 +17,31 @@ def create_task(request):
     task.task_text=task_text
     deadline = request.POST['deadline']
     task.deadline=deadline
+    task.save()
+    return redirect(index)
+
+def complete_task(request,id):
+    task=Task.objects.get(id=id)
+    task.is_finished = True
+    task.save()
+    return redirect(index)
+
+def delete(request,id):
+    task=Task.objects.get(id=id)
+    task.delete()
+    return redirect(index)
+
+def edit(request,id):
+    task=Task.objects.get(id=id)
+    return render(request, 'edit.html', {'task':task})
+
+def edit_task(request,id):
+    task=Task.objects.get(id=id)
+    task_text = request.POST['task_text']
+    task.task_text=task_text
+    deadline = request.POST['deadline']
+    task.deadline=deadline
+    is_finished = request.POST['is_finished']
+    task.is_finished=is_finished
     task.save()
     return redirect(index)
