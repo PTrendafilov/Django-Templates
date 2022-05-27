f6370b5a15ec247e6dd082e222a7a72c09746c72
PTrendafilov <80783727+PTrendafilov@users.noreply.github.com>
2022-05-27 21:18:24 +0300

    Add files via upload
    All files for the project was created. Also now you can save users in the database.

--- user-functionality/accounts/views.py
+++ user-functionality/accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User, auth
 # Create your views here.
+def index(request):
+    return render(request, 'registration.html')
+def registrate(request):
+    user = User()
+    user.first_name = request.POST['first_name']
+    user.last_name = request.POST['last_name']
+    user.email = request.POST['email']
+    user.username = request.POST['email']
+    user.password = request.POST['password']
+    user.save()
+    return redirect(index)
--- user-functionality/settings/settings.py
+++ user-functionality/settings/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'settings.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'User-Functionality',
+        'USER': 'postgres',
+        'PASSWORD':'777777Plamen',
+        'HOST': 'localhost',
+        'PORT':'5432',
     }
 }
 
